97ee6b6fe7dfd7ce34f2d3cc4c1dfc3adf322c3a
Pooja-manral <manralpooja2002@gmail.com>
2025-04-30 09:45:24 +0530

    add backend

---
+++ Backend/database.js
@@ -0,0 +1,11 @@
+const mongoose  =require('mongoose')
+async function dbconn(){
+    const conn=await mongoose.connect('mongodb+srv://Pooja:root@cluster0.aetkfm1.mongodb.net/student?retryWrites=true&w=majority&appName=Cluster0');
+    if(conn){
+        console.log('database connect successfully')
+    }
+    else{
+        console.log('connection fail');
+    }
+}
+module.exports=dbconn;
